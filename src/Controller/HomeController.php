14fbe7a38a8fb3aaa437bfae04b61def773a2ffc
MoaazKHASSAWNEH <moath.o.h.k@gmail.com>
2021-10-05 14:06:37 +0200

    Ajouter des methodes sur HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,4 +17,29 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    public function apropos(): Response 
+    {
+        return $this->render('home/apropos.html.twig', [
+            'controller_name' => 'HomeController' ,
+        ]);
+    }
+
+    public function contact(): Response 
+    {
+        return $this->render('home/contact.html.twig', [
+            'controller_name' => 'HomeController' ,
+        ]);
+    }
+
+    /**
+     * @Route("/programme", name="programme")
+     */
+
+    public function programme()
+    {
+        return $this->render('home/programme.html.twig' , [
+            'controller_nam' => 'HomeController',
+        ]); 
+    }
 }
