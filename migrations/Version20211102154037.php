23976c773fee808d3c0034f65a8dcdd6bcd6ee9e
MoaazKHASSAWNEH <moath.o.h.k@gmail.com>
2021-11-02 17:04:27 +0100

    Après la supprition de la bdd et les migrations et tout refaire

---
+++ migrations/Version20211102154037.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211102154037 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, contenu VARCHAR(500) NOT NULL, date DATE NOT NULL, resume VARCHAR(255) NOT NULL, image VARCHAR(500) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, resume VARCHAR(750) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL, titre VARCHAR(255) NOT NULL, categorie VARCHAR(255) NOT NULL, image VARCHAR(500) DEFAULT NULL, description VARCHAR(1000) NOT NULL, valeur VARCHAR(500) NOT NULL, adresse VARCHAR(750) NOT NULL, accessibility VARCHAR(500) NOT NULL, statut VARCHAR(255) NOT NULL, premiere_page TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, photo VARCHAR(500) DEFAULT NULL, date_naissance DATE NOT NULL, login VARCHAR(255) NOT NULL, password VARCHAR(500) NOT NULL, adresse VARCHAR(700) NOT NULL, email VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE article');
+        $this->addSql('DROP TABLE categorie');
+        $this->addSql('DROP TABLE location');
+        $this->addSql('DROP TABLE utilisateur');
+    }
+}
